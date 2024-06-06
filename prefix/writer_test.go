061a60a360d94605025334a05ed118d69e9e1bb4
Jordan Hasgul <jordan.hasgul@transferwise.com>
2024-06-06 16:18:43 +0300

    bench: add benchmark for write
    Signed-off-by: Jordan Hasgul <jordan.hasgul@transferwise.com>

--- prefix/writer_test.go
+++ prefix/writer_test.go
@@ -1,19 +1,23 @@
-package prefix
+package prefix_test
 
 import (
+	"fmt"
+	"os"
 	"strings"
 	"testing"
+	"unsafe"
 
+	"github.com/jordanhasgul/multierr/prefix"
 	"github.com/stretchr/testify/require"
 )
 
-func TestWriteSomething(t *testing.T) {
-	t.Run("write-nothing", func(t *testing.T) {
+func TestWrite(t *testing.T) {
+	t.Run("write nothing", func(t *testing.T) {
 		var (
 			want = ""
 
 			sb strings.Builder
-			w  = New(&sb, "* ")
+			w  = prefix.New(&sb, prefix.WithPrefix("* "))
 		)
 		_, _ = w.Write([]byte(""))
 		got := sb.String()
@@ -21,7 +25,7 @@ func TestWriteSomething(t *testing.T) {
 		require.Equal(t, want, got)
 	})
 
-	t.Run("write-something", func(t *testing.T) {
+	t.Run("write something", func(t *testing.T) {
 		var (
 			want = "" +
 				"* item 1\n" +
@@ -29,7 +33,7 @@ func TestWriteSomething(t *testing.T) {
 				"* item 3"
 
 			sb strings.Builder
-			w  = New(&sb, "* ")
+			w  = prefix.New(&sb, prefix.WithPrefix("* "))
 		)
 		_, _ = w.Write([]byte("item 1\n"))
 		_, _ = w.Write([]byte("item 2\n"))
@@ -38,4 +42,28 @@ func TestWriteSomething(t *testing.T) {
 
 		require.Equal(t, want, got)
 	})
+}
+
+func BenchmarkWrite(b *testing.B) {
+	b.ReportAllocs()
+
+	file, _ := os.CreateTemp("", "write_benchmark")
+	defer os.Remove(file.Name())
+
+	items := make([]string, 0, 100)
+	for idx := range len(items) {
+		item := fmt.Sprintf("item %d\n", idx)
+		items = append(items, item)
+	}
+
+	writer := prefix.New(file, prefix.WithPrefix("* "))
+	for n := 0; n < b.N; n++ {
+		for _, item := range items {
+			itemBytes := unsafe.Slice(
+				unsafe.StringData(item),
+				len(item),
+			)
+			_, _ = writer.Write(itemBytes)
+		}
+	}
 }

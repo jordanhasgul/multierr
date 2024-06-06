61925f1a928525da0ceedac20e07ca98ceba56ec
Jordan Hasgul <jordan.hasgul@transferwise.com>
2024-06-06 16:23:02 +0300

    fix: use new prefix writer api
    Signed-off-by: Jordan Hasgul <jordan.hasgul@transferwise.com>

--- multierr.go
+++ multierr.go
@@ -94,8 +94,8 @@ func fprintError(currWriter, prevWriter io.Writer, e *Error) {
 		switch err := err.(type) {
 		case *Error:
 			var (
-				prevWriter = prefix.New(currWriter, pipe)
-				currWriter = prefix.New(currWriter, sep)
+				prevWriter = prefix.New(currWriter, prefix.WithPrefix(pipe))
+				currWriter = prefix.New(currWriter, prefix.WithPrefix(sep))
 			)
 			fprintError(currWriter, prevWriter, err)
 		default:
